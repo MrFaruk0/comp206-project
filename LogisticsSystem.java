3535806c957360d05f035c74a8bcc8ec6d68fe59
1-MaSK <muhammedstkck@gmail.com>
2026-05-03 17:19:29 +0300

    added package and some template files

---
+++ LogisticsSystem.java
@@ -0,0 +1,30 @@
+/*public class LogisticsSystem {
+    private SinglyLinkedList masterRegistry;
+    private DoublyLinkedList intakeBuffer;
+    private PackageQueue standardDelivery;
+    private PackageStack truckLoading;
+    private AVLTree addressDirectory;
+    private Graph cityMap;
+
+    public LogisticsSystem() {
+        // Initialize all data structures
+        masterRegistry = new SinglyLinkedList();
+        intakeBuffer = new DoublyLinkedList();
+        standardDelivery = new PackageQueue();
+        truckLoading = new PackageStack();
+        addressDirectory = new AVLTree();
+        cityMap = new Graph();
+    }
+
+    public void start() {
+        System.out.println("Initializing Logistics System...");
+        // Load data from files and demonstrate operations
+        
+        // TODO: Load mapData.txt via MapLoader
+        // TODO: Load packageData.txt via PackageLoader
+        // TODO: Demonstrate each component
+        
+        System.out.println("\nSystem ready for operations.");
+    }
+}
+*/
